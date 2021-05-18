2b0605b9c0f3a7e917b4f055236286fb0c39a9b1
Zachary Napfel <znapfel@gmail.com>
2021-05-18 18:07:39 -0400

    privateConfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  publicRuntimeConfig: {
+  privateRuntimeConfig: {
     githubToken: process.env.GITHUB_TOKEN,
   },
 
